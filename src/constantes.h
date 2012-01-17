ea8b767bbe115097e045bf30f013e9a7d6b2e544
Schada <schada@orange.fr>
2012-01-17 19:22:17 +0100

    Ajout des classes de bases.

---
+++ src/constantes.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+#define DOSSIER_DONNEES "Data\\"
+#define FICHIER_DEFAUT "Data\\Defaut.dat"
+#define FICHIER_CIV_DEFAUT "Civilisation.dat"
+#define FIN_DOSSIER "\\"
+
+#endif // CONSTANTES_H
